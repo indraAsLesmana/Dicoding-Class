5908ec803fce0f3d2a2e5bcb2ec46b5e8ec55a7b
indralesmana <indra953@gmail.com>
2018-11-04 20:48:50 +0700

    - add insert method to... option detail activity

--- app/src/main/java/com/tutor93/menampilkanarray/detailview/DetailTeam.kt
+++ app/src/main/java/com/tutor93/menampilkanarray/detailview/DetailTeam.kt
@@ -1,21 +1,29 @@
 package com.tutor93.menampilkanarray.detailview
 
+import android.database.sqlite.SQLiteConstraintException
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.Menu
 import android.view.MenuItem
-import android.widget.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import com.tutor93.menampilkanarray.*
 import com.tutor93.menampilkanarray.R.color.colorAccent
 import com.tutor93.menampilkanarray.R.color.colorSecondaryText
 import com.tutor93.menampilkanarray.api.ApiRepository
+import com.tutor93.menampilkanarray.data.Favorite
 import com.tutor93.menampilkanarray.model.Team
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
@@ -114,12 +122,39 @@ class DetailTeam : AppCompatActivity(), DetailTeamView {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail_menu, menu)
+        //menuItem = menu
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return if (item?.itemId == android.R.id.home){
-            finish()
-            false
-        }else{
-            super.onOptionsItemSelected(item)
+
+        return when(item?.itemId){
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            R.id.add_to_favorite -> {
+                addtoFavorite()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun addtoFavorite() {
+        try {
+            database.use {
+                insert(
+                    Favorite.TABLE_FAVORITE,
+                    Favorite.TEAM_ID to mTeam.teamId,
+                    Favorite.TEAM_NAME to mTeam.teamName,
+                    Favorite.TEAM_BADGE to mTeam.teamBadge)
+            }
+            snackbar(swipeRefresh, "Added to favorite").show()
+        } catch (e: SQLiteConstraintException){
+            snackbar(swipeRefresh, e.localizedMessage).show()
         }
     }
 
@@ -131,9 +166,9 @@ class DetailTeam : AppCompatActivity(), DetailTeamView {
     }
 
     override fun showTeamList(data: List<Team>) {
-       /* val teams = Team(data[0].teamId,
+        mTeam = Team(data[0].teamId,
             data[0].teamName,
-            data[0].teamBadge)*/
+            data[0].teamBadge)
         swipeRefresh.isRefreshing = false
         Picasso.get().load(data[0].teamBadge).into(teamBadge)
         teamName.text = data[0].teamName
