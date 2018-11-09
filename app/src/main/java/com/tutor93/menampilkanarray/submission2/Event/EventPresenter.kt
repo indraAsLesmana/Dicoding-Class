7bf07cf16ff4a4fa8dbc6fe0a66e20a1af78fffe
indra agus lesmana <indra953@gmail.com>
2018-11-10 05:47:47 +0700

    - implement courotines

--- app/src/main/java/com/tutor93/menampilkanarray/submission2/Event/EventPresenter.kt
+++ app/src/main/java/com/tutor93/menampilkanarray/submission2/Event/EventPresenter.kt
@@ -4,27 +4,29 @@ import com.google.gson.Gson
 import com.tutor93.menampilkanarray.api.ApiRepository
 import com.tutor93.menampilkanarray.model.TheSportDBApi
 import com.tutor93.menampilkanarray.model.response.MatchResponse
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 
 class EventPresenter(private val view: EventView,
                      private val apiRepository: ApiRepository,
                      private val gson: Gson
 ) {
     fun getMatchList(leagueId: String?, isPastRequest: Boolean? = false) {
+        /**
+         * with courutines
+         * */
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(
-                apiRepository
-                    .doRequest(TheSportDBApi.getMatchList(leagueId, isPastRequest)), MatchResponse::class.java
-            )
-            uiThread {
-                view.hideLoading()
-                data.events?.let {
-                    view.showMatchList(it)
-                }
-
+        async(UI) {
+            val data = bg {
+                gson.fromJson(
+                    apiRepository.doRequest(TheSportDBApi.getMatchList(leagueId, isPastRequest)),
+                    MatchResponse::class.java
+                )
             }
+            data.await().events?.let { view.showMatchList(it) }
+            view.hideLoading()
+
         }
     }
 }
